d8b2292338967ed3b219c8d49a9b3c52617ea192
Araceli Valdovinos <valdovinosaraceli50@gmail.com>
2021-06-13 14:12:23 -0500

    WOP: recognize and replace 1 with Beep! I am unsure how to join the two functions together

--- js/scripts.js
+++ js/scripts.js
@@ -1,17 +1,43 @@
 //UI Logic
 $("document").ready(function(){
-$("form#formOne").submit(function(event){
-  event.preventDefault();
-  const text = $("input#word").val()
-  
-  $("#array").text(text)
+  $("form#formOne").submit(function(event){
+    event.preventDefault();
+    const number = $("input#number").val()
+    const arrayOutput = beepBoop(number)
 
-})
+    $("#array").text(arrayOutput)
+    
+  });
 });
 
+//Business Logic
 
+function beepBoop (string) {
+  const number = parseInt(string)
+  const output = [ ]
+  for (let i=0; i <=number; i+=1) {
+  output.push(i)
+  }
+  return output
+}
 
-
+function beepBoop (numbers) {
+  const numbersArray = numbers.split();
+  const selectNumbers = ["1", "2", "3"]
+  let word= []
+  for (let i=0; i < selectNumbers.length; i++) {
+    if (numbers.includes(selectNumbers[2])) {
+      return word = "Won't you be my neighbor?"
+    }
+    else if (numbers.includes(selectNumbers[1])){
+      return word = "Boop!"
+    }
+    else if (numbers.includes(selectNumbers[0])) {
+      return word = "Beep!"
+    }
+  }
+  return numbersArray
+}
 
 
 
